98ac372db57fad3641b9be235c5b0dc8ee0be877
Andrew Miotke <andrewmiotke@gmail.com>
2022-01-11 12:47:46 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
-import os 
+import os
+import argparse
 
 def main():
+    # Creates the argument parser
+    parser = argparse.ArgumentParser(description="Argparse learning CLI tool")
+
+    # Available arguments. All arguments are optional
+    parser.add_argument("stuff", nargs="?", help="Helps create Okta groups used as Google groups")
+    
+    if sys.argv[1] == "stuff":
+        do_stuff()
+        
+    args = parser.parse)args()
+
+def do_stuff():
+    print("is stuff happening?")
     for i in range(1, 10):
         print(i)
-    
-    os.system("echo hello, actions")
-
+        
 if __name__ == "__main__":
     main()
