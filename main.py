9841b261306e1b81d89eef56dc84f826ac2adeb3
Andrew Miotke <andrewmiotke@gmail.com>
2022-01-11 12:10:44 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def main():
-    print("Hello, GitHub Actions")
+import os 
 
+def main():
+    for i in range(1, 10):
+        print(i)
+    
+    os.system("echo hello, actions")
 
 if __name__ == "__main__":
     main()
