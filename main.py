ce1b64f43ed5c3e2ef03d40fcb51960d8ae1d3a6
Andrew Miotke <amiotke@karat.com>
2022-01-11 12:50:22 -0800

    Update script

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import argparse
 
 def main():
@@ -7,16 +8,16 @@ def main():
 
     # Available arguments. All arguments are optional
     parser.add_argument("stuff", nargs="?", help="Helps create Okta groups used as Google groups")
-    
+
     if sys.argv[1] == "stuff":
         do_stuff()
-        
-    args = parser.parse)args()
+
+    args = parser.parse_args()
 
 def do_stuff():
     print("is stuff happening?")
     for i in range(1, 10):
         print(i)
-        
+
 if __name__ == "__main__":
     main()
